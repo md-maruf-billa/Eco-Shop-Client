266faf276f613cf79def44bbff9268a26e5b66e5
Rebakum <anamikaeasmin@hotmail.com>
2024-07-13 20:37:28 +0600

    add: added bannar

---
+++ src/Components/Bannar/Bannar.jsx
@@ -0,0 +1,11 @@
+
+
+const Bannar = () => {
+    return (
+        <div>
+          <h1>This is a Bannar</h1>  
+        </div>
+    );
+};
+
+export default Bannar;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+
+import Bannar from '../../Components/Bannar/Bannar';
 
 const Home = () => {
     return (
         <div>
             This is home
+            <Bannar/>
         </div>
     );
 };
